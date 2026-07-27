7cdb88da2ff7b8308efe7b969839653847caa12e
Paraag Harish Gulve <gulveparag8@gmail.com>
2026-07-27 20:04:36 +0530

    Changes in the main.py about the FastApi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,35 @@
 from fastapi import FastAPI
 
+# Create FastAPI application
 app = FastAPI(
     title="PayShield API",
-    description="AI-Powered Banking Transaction Simulation & Fraud Detection Platform",
+    description="AI Powered Banking Transaction Simulation Platform",
     version="1.0.0"
 )
 
+# Home API
 @app.get("/")
-def root():
+def home():
     return {
         "message": "Welcome to PayShield API 🚀",
-        "status": "Running Successfully"
+        "status": "Server Running"
+    }
+
+# Health Check API
+@app.get("/health")
+def health():
+    return {
+        "status": "Healthy",
+        "server": "Running",
+        "database": "Not Connected Yet"
+    }
+
+# About API
+@app.get("/about")
+def about():
+    return {
+        "project": "PayShield",
+        "developer": "Parag Gulve",
+        "version": "1.0",
+        "purpose": "Banking Transaction Simulation Platform"
     }
